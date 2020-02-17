e0defd17e4d011465b3e37aeb742fc862dcdafd9
Igor Gašparović <dragan.stanojevic@3cad.hr>
2020-02-17 11:27:06 +0100

    Sections name, main.js.

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    $(".navbar a, footer a[href='#home'], a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    var slideanim = function () {
+        $(".slideanim").each(function () {
+            var pos = $(this).offset().top;
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    }
+    slideanim();
+
+    $(window).scroll(function () {
+        slideanim();
+    });
+})
+
+function openNav() {
+    if (document.getElementById("mySidenav").style.width == "250px") {
+        document.getElementById("mySidenav").style.width = "0";
+    } else {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
